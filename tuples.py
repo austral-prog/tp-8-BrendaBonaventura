ff0a1cec7760f18ce753886d07db8fac2c611d32
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-05-29 19:53:51 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,11 +1,13 @@
 def get_coordinates():
 	esto=("Scrimshawed Whale Tooth","2A")
 	return esto[1]
+
 def convert_coordinate():
 	coor="2A"
 	c1=coor[0]
 	c2=coor[1]
 	return (f"{c1}",f"{c2}")
+
 def create_record():
 	azara=("Brass Spyglass","4B")
 	rui=("Abandoned Lighthouse","4B","Blue")
