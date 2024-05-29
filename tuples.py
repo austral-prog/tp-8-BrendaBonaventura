597c688e73de244d6819c045c82e9af3fff2449c
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-05-29 20:32:07 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,14 +1,14 @@
 def get_coordinate(x):
-	esto=("Scrimshawed Whale Tooth","2A")
+	esto=("Scrimshawed Whale Tooth","4B")
 	return esto[1]
 
 def convert_coordinate(x):
 	input_data=('2A', '4B', '1C', '6D', '7E', '7F','6A', '8A', '5B', '8C', '1F', '3D', '4E')
-	result_data=('2', 'A'),('4', 'B')
+	result_data=('2', 'A')
 	coor=result_data[0]
 	return result_data
 
-def create_record(x):
+def create_record(x,y):
 	azara=("Brass Spyglass","4B")
 	rui=("Abandoned Lighthouse","4B","Blue")
 	dos=azara[1]
