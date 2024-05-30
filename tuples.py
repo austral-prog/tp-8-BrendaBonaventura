e3c776cda2e45e4c4649e46db3843da959485d86
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-05-30 14:46:41 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,14 +1,10 @@
 def get_coordinate(record):
     record=record[1]
     return record
-
-
 def convert_coordinate(coordinate):
     c1=coordinate[0]
     c2=coordinate[1]
     return (c1,c2)
-
-
 def create_record(azara_record, rui_record):
     CA=convert_coordinate(get_coordinate(azara_record))
     if CA==rui_record[1]:
