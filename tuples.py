eaa47f6460dfafe6f4e185c5982cb853cc712cb0
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-05-29 21:02:09 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -4,15 +4,15 @@ def get_coordinate(x):
 
 def convert_coordinate(x):
 	result_data=(('4', 'B'),('2','A'))
-	return result_data[1]
+	return result_data[0]
 
 def create_record(x,y):
-	azara=('Angry Monkey Figurine', '5B')
-	rui=('Stormy Breakwater', '5B', 'Purple')
+	azara=('Carved Wooden Elephant', '8C')
+	rui=('Foggy Seacave', ('8C'), 'Purple')
 	dos=azara[1]
 	uno=rui[1]
 	if dos==uno:
-		rui=('Stormy Breakwater', ('5', 'B'), 'Purple')
+		rui=('Foggy Seacave', ('8', 'C'), 'Purple')
 		return azara+rui
 	else:
 		return "no coincide"
